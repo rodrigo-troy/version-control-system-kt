060f19e1c658d3e39d2f4d010115bd0ce9aa8957
rodrigotroy <1328268+rodrigo-troy@users.noreply.github.com>
2023-03-11 21:55:58 -0300

    Stage 3

--- Version Control System/task/src/svcs/Main.kt
+++ Version Control System/task/src/svcs/Main.kt
@@ -4,8 +4,3 @@ fun main(args: Array<String>) {
     val versionControl = VersionControl()
     versionControl.execute(if (args.isNotEmpty()) args else arrayOf("/help"))
 }
-
-
-open class Animal
-open class Fish : Animal()
-class Salmon : Fish()
